fe40288ff7f1cc94c7de03ca661ed3a5c26164a2
sekiguchi0215 <dsphere5296@gmail.com>
2021-05-21 02:30:56 +0900

    beforeとmerge！の組み合わせ

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -3,8 +3,10 @@ require_relative "../lib/user"
 RSpec.describe User do
   describe "Userクラスの情報" do
     let(:params) {{name: "Tanaka", age: 31, hobby: "Soccer"}}
-    let(:params2) {{name: "Suzuki", age: 31, hobby: "Soccer"}}
     context "名前がTanakaの場合" do
+      before do
+        params.merge!(name: "Tanaka")
+      end
       it "return user name" do
         user = User.new(params)
         expect(user.name).to eq "Tanaka"
@@ -19,15 +21,18 @@ RSpec.describe User do
       end
     end
     context "名前がSuzukiの場合" do
+      before do
+        params.merge!(name: "Suzuki")
+      end
       it "return user name" do
-        user = User.new(params2)
+        user = User.new(params)
         expect(user.name).to eq "Suzuki"
       end
       it "return user age" do
-        user = User.new(params2)
+        user = User.new(params)
       end
       it "return user hobby" do
-        user = User.new(params2)
+        user = User.new(params)
         expect(user.hobby).to eq "Soccer"
       end
     end

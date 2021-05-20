dd030c829c20d3a8c5a87ad1bcbcc3711cfa85af
sekiguchi0215 <dsphere5296@gmail.com>
2021-05-21 02:34:42 +0900

    letを利用したオブジェクトの変数化

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -2,21 +2,19 @@ require_relative "../lib/user"
 
 RSpec.describe User do
   describe "Userクラスの情報" do
-    let(:params) {{name: "Tanaka", age: 31, hobby: "Soccer"}}
+    let(:user) { User.new(params) }
+    let(:params) {{age: 31, hobby: "Soccer"}}
     context "名前がTanakaの場合" do
       before do
         params.merge!(name: "Tanaka")
       end
       it "return user name" do
-        user = User.new(params)
         expect(user.name).to eq "Tanaka"
       end
       it "return user age" do
-        user = User.new(params)
         expect(user.age).to eq 31
       end
       it "return user hobby" do
-        user = User.new(params)
         expect(user.hobby).to eq "Soccer"
       end
     end
@@ -25,14 +23,12 @@ RSpec.describe User do
         params.merge!(name: "Suzuki")
       end
       it "return user name" do
-        user = User.new(params)
         expect(user.name).to eq "Suzuki"
       end
       it "return user age" do
-        user = User.new(params)
+        expect(user.age).to eq 31
       end
       it "return user hobby" do
-        user = User.new(params)
         expect(user.hobby).to eq "Soccer"
       end
     end

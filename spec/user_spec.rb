c902af4ad870b3761b5ea687437db54bce7dea24
sekiguchi0215 <dsphere5296@gmail.com>
2021-05-21 02:18:45 +0900

    contextを使うパターン

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -2,17 +2,33 @@ require_relative "../lib/user"
 
 RSpec.describe User do
   describe "Userクラスの情報" do
-    it "return user name" do
-      user = User.new(name: "Tanaka", age: 31, hobby: "Soccer")
-      expect(user.name).to eq "Tanaka"
+    context "名前がTanakaの場合" do
+      it "return user name" do
+        user = User.new(name: "Tanaka", age: 31, hobby: "Soccer")
+        expect(user.name).to eq "Tanaka"
+      end
+      it "return user age" do
+        user = User.new(name: "Tanaka", age: 31, hobby: "Soccer")
+        expect(user.age).to eq 31
+      end
+      it "return user hobby" do
+        user = User.new(name: "Tanaka", age: 31, hobby: "Soccer")
+        expect(user.hobby).to eq "Soccer"
+      end
     end
-    it "return user age" do
-      user = User.new(name: "Tanaka", age: 31, hobby: "Soccer")
-      expect(user.age).to eq 31
-    end
-    it "return user hobby" do
-      user = User.new(name: "Tanaka", age: 31, hobby: "Soccer")
-      expect(user.hobby).to eq "Soccer"
+    context "名前がSuzukiの場合" do
+      it "return user name" do
+        user = User.new(name: "Suzuki", age: 31, hobby: "Soccer")
+        expect(user.name).to eq "Suzuki"
+      end
+      it "return user age" do
+        user = User.new(name: "Suzuki", age: 31, hobby: "Soccer")
+        expect(user.age).to eq 31
+      end
+      it "return user hobby" do
+        user = User.new(name: "Suzuki", age: 31, hobby: "Soccer")
+        expect(user.hobby).to eq "Soccer"
+      end
     end
   end
 end

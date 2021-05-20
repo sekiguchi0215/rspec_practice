1842ec249540ebf9908046329fce6eba0a1fcf93
sekiguchi0215 <dsphere5296@gmail.com>
2021-05-21 02:25:25 +0900

    letを使うパターン

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -2,36 +2,32 @@ require_relative "../lib/user"
 
 RSpec.describe User do
   describe "Userクラスの情報" do
+    let(:params) {{name: "Tanaka", age: 31, hobby: "Soccer"}}
+    let(:params2) {{name: "Suzuki", age: 31, hobby: "Soccer"}}
     context "名前がTanakaの場合" do
-      before do
-        @params = {name: "Tanaka", age: 31, hobby: "Soccer"}
-      end
       it "return user name" do
-        user = User.new(@params)
+        user = User.new(params)
         expect(user.name).to eq "Tanaka"
       end
       it "return user age" do
-        user = User.new(@params)
+        user = User.new(params)
         expect(user.age).to eq 31
       end
       it "return user hobby" do
-        user = User.new(@params)
+        user = User.new(params)
         expect(user.hobby).to eq "Soccer"
       end
     end
     context "名前がSuzukiの場合" do
-      before do
-        @params2 = {name: "Suzuki", age: 31, hobby: "Soccer"}
-      end
       it "return user name" do
-        user = User.new(@params2)
+        user = User.new(params2)
         expect(user.name).to eq "Suzuki"
       end
       it "return user age" do
-        user = User.new(@params2)
+        user = User.new(params2)
       end
       it "return user hobby" do
-        user = User.new(@params2)
+        user = User.new(params2)
         expect(user.hobby).to eq "Soccer"
       end
     end
